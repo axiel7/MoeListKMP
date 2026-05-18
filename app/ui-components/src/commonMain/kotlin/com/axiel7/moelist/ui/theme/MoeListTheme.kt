195a5ae4a9eaa9aa210c6139638e78c9e27fda13
axiel7 <axiel7lopez@gmail.com>
2026-05-18 10:58:45 +0200

    chore: remove LocalAppTheme

--- app/ui-components/src/commonMain/kotlin/com/axiel7/moelist/ui/theme/MoeListTheme.kt
+++ app/ui-components/src/commonMain/kotlin/com/axiel7/moelist/ui/theme/MoeListTheme.kt
@@ -4,19 +4,11 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ProvidedValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import com.materialkolor.PaletteStyle
 import com.materialkolor.dynamicColorScheme
 import com.materialkolor.dynamiccolor.ColorSpec
-
-// Exposes custom theme value to Compose resources, https://youtrack.jetbrains.com/issue/CMP-4197
-expect object LocalAppTheme {
-    val current: Boolean @Composable get
-    @Composable
-    infix fun provides(value: Boolean?): ProvidedValue<*>
-}
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
