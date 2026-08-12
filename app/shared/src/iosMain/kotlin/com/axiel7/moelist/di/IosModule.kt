49644b2389e8bbe6058a25bf3cbd91c5970e4874
Axel <axiel7lopez@gmail.com>
2026-08-12 12:47:07 +0200

    fix(ios): migrate kmpworkmanager 3.3.0

--- app/shared/src/iosMain/kotlin/com/axiel7/moelist/di/IosModule.kt
+++ app/shared/src/iosMain/kotlin/com/axiel7/moelist/di/IosModule.kt
@@ -4,16 +4,21 @@ import com.axiel7.moelist.data.utils.NOTIFICATIONS_DATA_STORE
 import com.axiel7.moelist.screens.details.topbar.MediaNotificationsViewModel
 import com.axiel7.moelist.screens.more.notifications.NotificationsViewModel
 import com.axiel7.moelist.worker.DefaultWorkerFactory
-import dev.brewkits.kmpworkmanager.kmpWorkerModule
+import dev.brewkits.kmpworkmanager.KmpWorkManager
+import dev.brewkits.kmpworkmanager.background.data.ChainExecutor
+import dev.brewkits.kmpworkmanager.background.data.DynamicTaskDispatcher
+import dev.brewkits.kmpworkmanager.background.domain.BackgroundTaskScheduler
 import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val iosModule = module {
-    includes(kmpWorkerModule(
-        workerFactory = DefaultWorkerFactory()
-    ))
+    KmpWorkManager.initialize(workerFactory = DefaultWorkerFactory())
+
+    single<BackgroundTaskScheduler> { KmpWorkManager.getInstance().backgroundTaskScheduler }
+    single<ChainExecutor> { KmpWorkManager.getInstance().chainExecutor }
+    single<DynamicTaskDispatcher> { KmpWorkManager.getInstance().dynamicTaskDispatcher }
 }
 
 val iosViewModelModule = module {
